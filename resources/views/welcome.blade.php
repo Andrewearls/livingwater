358effd6f86cb9da0a1effef0d12997b7b6b3b74
Andrew Earls <andrewearls@rocketmail.com>
2018-11-18 11:05:50 -0800

    verse of the day large image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,14 @@
     <div class="title">
         Verse of the Day
     </div>
-    <script src="https://www.biblegateway.com/votd/votd.write.callback.js"></script>
-    <script src="https://www.biblegateway.com/votd/get/?format=json&version=ESV&callback=BG.votdWriteCallback"></script>
-    <!-- alternative for no javascript -->
-    <noscript>
-    <iframe framespacing="0" frameborder="no" src="https://www.biblegateway.com/votd/get/?format=html&version=ESV">View Verse of the Day</iframe> 
-    </noscript>
+    <div id="verseoftheday">
+        <script src="https://www.biblegateway.com/votd/votd.write.callback.js"></script>
+        <script src="https://www.biblegateway.com/votd/get/?format=json&version=ESV&callback=BG.votdWriteCallback"></script>
+        <!-- alternative for no javascript -->
+        <noscript>
+        <iframe framespacing="0" frameborder="no" src="https://www.biblegateway.com/votd/get/?format=html&version=ESV">View Verse of the Day</iframe> 
+        </noscript>
+    </div>
 @endsection
 
 @section('content')
