67eee634d030c0437c8ccfe5c68c2914ed96d9df
Andrew Earls <andrewearls@rocketmail.com>
2018-12-03 12:53:43 -0800

    calendar update

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -5,5 +5,5 @@
 @endsection
 
 @section('content')
-	<iframe src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=nowwerecooking.io_hd92ve16ni7jubnjf0l7j90r64%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=America%2FLos_Angeles" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+	<iframe src="https://calendar.google.com/calendar/b/1/embed?showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=nowwerecooking.io_bkbi5ri6p65e3ir9dnm60bpf3k%40group.calendar.google.com&amp;color=%23333333&amp;ctz=America%2FLos_Angeles" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 @endsection
